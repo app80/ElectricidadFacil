fe371c88c3d6302d2b18c7c3d234eac9ecb5bb54
app80 <innovapp80@gmail.com>
2025-07-17 23:44:27 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-window.addEventListener("load",function(){
-  
-    document.getElementById("movil-menu").hidden = true;
+window.addEventListener("load", function () {
+  document.getElementById("movil-menu").hidden = true;
+});
 
-}); 
+// Esperar a que la página cargue completamente
+document.addEventListener("DOMContentLoaded", function () {
+  const cookieBanner = document.getElementById("cookie-banner");
+  const acceptButton = document.getElementById("accept-button");
+});
 
-
-hamburguesa.addEventListener("click",function(){
-
+hamburguesa.addEventListener("click", function () {
   var Visible = document.getElementById("movil-menu").hidden;
-  var intro = document.getElementById('movil-menu');
- 
+  var intro = document.getElementById("movil-menu");
 
-  if (Visible == true){
+  if (Visible == true) {
     document.getElementById("movil-menu").hidden = false;
-    intro.className = 'movil-menu';
+    intro.className = "movil-menu";
   } else {
     document.getElementById("movil-menu").hidden = true;
-    intro.className = 'menu-oculto';
+    intro.className = "menu-oculto";
   }
-
-}); 
+});
